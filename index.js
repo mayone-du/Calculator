4f1858e832e12dff1adb438fcc4c437045723612
mayone-du <cocomayo1201@icloud.com>
2021-01-07 12:38:36 +0900

    ACボタンを押したときの処理を追加

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
 (() => {
 
+
+  class Calculator {
+    constructor($prevOperand, $currOperand) {
+      this.$prevOperand = $prevOperand;
+      this.$currOperand = $currOperand;
+    }
+
+    clear() {
+      this.$prevOperand.innerText = "";
+      this.$currOperand.innerText = "";
+    }
+
+    delete() {
+
+    }
+
+    chooseOpration() {
+
+    }
+
+    updateDisplay() {
+
+    }
+
+
+  }
+
   const $doc = document;
+  const $acBtn = $doc.querySelector("[data-clear]");
+  const $delBtn = $doc.querySelector("[data-delete]");
+  const $equalbtn = $doc.querySelector("[data-equal");
+  const $opeBtns = $doc.querySelectorAll("[data-operation]");
+  const $numBtns = $doc.querySelectorAll("[data-number]");
+  const $prevOperand = $doc.querySelector(".bl_output_prevOperand");
+  const $currOperand = $doc.querySelector(".bl_output_currOperand");
+
+
+
+  const calculator = new Calculator($prevOperand, $currOperand);
+
+
+
+  // ACボタンを押したときの処理
+  $acBtn.addEventListener("click", function() {
+    calculator.clear();
+  })
+
+
+
 
 })();
